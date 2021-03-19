ab96dfe4f99978923c31540681151609cd757bae
Patrick <patrickdiehl@lsu.edu>
2021-03-19 15:27:47 -0500

    Update the Neumann BC for the vhm at x = 3 only

--- coupling-vhm.py
+++ coupling-vhm.py
@@ -157,9 +157,11 @@ def CouplingFDFD(n,h):
         M[i][i] = 4
         M[i][i+1] = -2
 
-    M[3*n-1][3*n-1] = 3*h
-    M[3*n-1][3*n-2] = -4*h
-    M[3*n-1][3*n-3] = h
+    M[3*n-1][3*n-1] = 11*h / 3
+    M[3*n-1][3*n-2] = -18*h / 3
+    M[3*n-1][3*n-3] = 9 * h / 3
+    M[3*n-1][3*n-4] = -2 * h / 3
+
 
     M *= 1./(2.*h*h)
 
@@ -260,9 +262,10 @@ def CouplingFDVHM(n,h):
         M[i][i] = 4 * fFDM
         M[i][i+1] = -2 * fFDM
 
-    M[3*n-1][3*n-1] = 3*h * fFDM
-    M[3*n-1][3*n-2] = -4*h * fFDM
-    M[3*n-1][3*n-3] = h * fFDM
+    M[3*n-1][3*n-1] = 11*h * fFDM / 3
+    M[3*n-1][3*n-2] = -18*h * fFDM / 3 
+    M[3*n-1][3*n-3] = 9 * h * fFDM / 3
+    M[3*n-1][3*n-4] = -2 * h * fFDM / 3
     
     return M
 
