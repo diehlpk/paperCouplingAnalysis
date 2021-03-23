311aebabbf9a5afb83c6a877fb27382510bb97ef
Patrick <patrickdiehl@lsu.edu>
2021-03-22 21:41:36 -0500

    Correct code for linear case for approach 1 m-convergence

--- coupling-approach-1-convergence.py
+++ coupling-approach-1-convergence.py
@@ -343,6 +343,128 @@ def Coupling4(n,h):
     return M
 
 
+def Coupling8(n,h):
+
+    M = np.zeros([3*n+16,3*n+16])
+
+    fFD =  1./(2.*h*h)
+
+    # Boundary
+
+    M[0][0] = 1
+
+    # FD 
+
+    for i in range(1,n-1):
+        M[i][i-1] = -2 * fFD
+        M[i][i] = 4 * fFD
+        M[i][i+1] = -2 * fFD
+
+    # Overlapp
+
+    M[n-1][n-1] = -1
+    M[n-1][n+8] = 1
+
+    M[n][n] = -1
+    M[n][n-9] = 1
+
+    M[n+1][n+1] = -1
+    M[n+1][n-8] = 1
+
+    M[n+2][n+2] = -1
+    M[n+2][n-7] = 1
+
+    M[n+3][n+3] = -1
+    M[n+3][n-6] = 1
+
+    M[n+4][n+4] = -1
+    M[n+4][n-5] = 1
+
+    M[n+5][n+5] = -1
+    M[n+5][n-4] = 1
+
+    M[n+6][n+6] = -1
+    M[n+6][n-3] = 1
+
+    M[n+7][n+7] = -1
+    M[n+7][n-2] = 1
+
+    # PD
+
+    for i in range(n+8,2*n+8):
+        M[i][i-8] = -(1/512) / 2. / h / h*2
+        M[i][i-7] = -(1/224) / 2. / h / h*2
+        M[i][i-6] = -(1/192) / 2. / h / h*2
+        M[i][i-5] = -(1/160) / 2. / h / h*2
+        M[i][i-4] = -(1/128) / 2. / h / h*2
+        M[i][i-3] = -(1/96) / 2. / h / h*2
+        M[i][i-2] = -(1/64) / 2. / h / h*2
+        M[i][i-1] = -(1/32) / 2. / h / h*2
+        M[i][i] =  (1487/8960) / 2. / h / h*2
+        M[i][i+1] = -(1/32) / 2. / h / h*2
+        M[i][i+2] = -(1/64) / 2. / h / h*2
+        M[i][i+3] = -(1/96) / 2. / h / h*2
+        M[i][i+4] = -(1/128) / 2. / h / h*2
+        M[i][i+5] = -(1/160) / 2. / h / h*2
+        M[i][i+6] = -(1/192) / 2. / h / h*2
+        M[i][i+7] = -(1/224) / 2. / h / h*2
+        M[i][i+8] = -(1/512) / 2. / h / h*2
+
+    # Overlap
+
+    M[2*n+8][2*n+8] = -1
+    M[2*n+8][2*n+17] = 1
+
+    M[2*n+9][2*n+9] = -1
+    M[2*n+9][2*n+18] = 1
+
+    M[2*n+10][2*n+10] = -1
+    M[2*n+10][2*n+19] = 1
+
+    M[2*n+11][2*n+11] = -1
+    M[2*n+11][2*n+20] = 1
+
+    M[2*n+12][2*n+12] = -1
+    M[2*n+12][2*n+21] = 1
+
+    M[2*n+13][2*n+13] = -1
+    M[2*n+13][2*n+22] = 1
+
+    M[2*n+14][2*n+14] = -1
+    M[2*n+14][2*n+23] = 1
+
+    M[2*n+15][2*n+15] = -1
+    M[2*n+15][2*n+24] = 1
+
+    M[2*n+16][2*n+16] = -1
+    M[2*n+16][2*n+7] = 1
+
+    # FD
+
+    for i in range(2*n+17,3*n+15):
+        M[i][i-1] = -2 * fFD
+        M[i][i] = 4 * fFD
+        M[i][i+1] = -2 * fFD
+
+    # Boundary
+
+    #M[3*n+3][3*n+3] = 3*  h * fFD 
+    #M[3*n+3][3*n+2] = -4*h * fFD  
+    #M[3*n+3][3*n+1] = h * fFD 
+
+    M[3*n+15][3*n+15] = 11 *  h * fFD / 3
+    M[3*n+15][3*n+14] =  -18 * h * fFD  / 3
+    M[3*n+15][3*n+13] = 9 * h * fFD / 3
+    M[3*n+15][3*n+12] = -2 * h * fFD / 3
+
+    np.savetxt("foo.csv", M, delimiter=",")
+
+    #plt.matshow(M)
+    #plt.show()
+
+    return M
+
+
 markers = ['s','o','x','.']
 
 
@@ -415,24 +537,19 @@ nodes = int(1 / h) + 1
 nodesFull = 3 * nodes
 
 x1 = np.linspace(0,1,nodes)
-x2 = np.linspace(1,2.,nodes)
+x2 = np.linspace(1-8*h,2+8*h,nodes+16)
 x3 = np.linspace(2,3.,nodes)
 x = np.array(np.concatenate((x1,x2,x3)))
 
-#print(nodesFull)
-#xFull = np.linspace(0,3.,nodesFull-2)
-#forceCoupled = forceCoupling(nodes,x)
-#forceCoupled[nodes-1] = 0
-#forceCoupled[nodes] = 0
+xFull = np.linspace(0,3.,nodesFull-2)
+forceCoupled = forceCoupling(nodes,x,8)
 
-#forceCoupled[2*nodes-1] = 0
-#forceCoupled[2*nodes] = 0
 
  
-#uFDMVHM = solve(CouplingFDVHM(nodes,h),forceCoupled)
-uSlice = np.array(np.concatenate((uFDMVHM[0:nodes-1],uFDMVHM[nodes:2*nodes-1],uFDMVHM[2*nodes:3*nodes])))
+uFDMVHM = solve(Coupling8(nodes,h),forceCoupled)
+#uSlice = np.array(np.concatenate((uFDMVHM[0:nodes-1],uFDMVHM[nodes:2*nodes-1],uFDMVHM[2*nodes:3*nodes])))
 
-#plt.plot(xFull,uSlice-exactSolution(xFull),c="black",label="m=8",marker=markers[2],markevery=5)
+plt.plot(x,uFDMVHM,c="black",label="m=8",marker=markers[2],markevery=5)
 
 plt.title("Example with "+example+" solution for Problem (17)")
 plt.legend()
