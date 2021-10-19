818605f98cd79c48c6623ca7eb5c7ebbe51f86c2
Patrick Diehl <me@diehlpk.de>
2021-10-19 16:15:44 -0500

    Update the plots

--- coupling-vhm-moving.py
+++ coupling-vhm-moving.py
@@ -77,7 +77,7 @@ def forceFull(n,h):
     for i in range(1,n-1):
         force[i] = f(i * h)
     
-    force[n-1] = g
+    force[n-1] = 0 #g
     
     return force
 
@@ -88,7 +88,7 @@ def forceCoupling(n,x):
     for i in range(1,n-1):
         force[i] = f(x[i])
     
-    force[n-1] = g
+    force[n-1] = 0 #g
     
     return force
 
@@ -135,10 +135,11 @@ def FDM(n,h):
         M[i][i] = 4
         M[i][i+1] = -2
 
-    M[n-1][n-1] = 11*h / 3 
-    M[n-1][n-2] = -18*h / 3
-    M[n-1][n-3] = 9* h / 3
-    M[n-1][n-4] = -2* h / 3
+    M[n-1][n-1] = 1
+    #M[n-1][n-1] = 11*h / 3 
+    #M[n-1][n-2] = -18*h / 3
+    #M[n-1][n-3] = 9* h / 3
+    #M[n-1][n-4] = -2* h / 3
 
 
     M *= 1./(2.*h*h)
@@ -308,11 +309,12 @@ def CouplingFDVHM(nodes1,nodes2,nodes3,h):
         M[i][i+1] = -2 * fFDM
 
     n += nodes3
-
-    M[n-1][n-1] = 11*h * fFDM / 3
-    M[n-1][n-2] = -18*h * fFDM / 3 
-    M[n-1][n-3] = 9 * h * fFDM / 3
-    M[n-1][n-4] = -2 * h * fFDM / 3
+    
+    M[n-1][n-1] = 1 
+    #M[n-1][n-1] = 11*h * fFDM / 3
+    #M[n-1][n-2] = -18*h * fFDM / 3 
+    #M[n-1][n-3] = 9 * h * fFDM / 3
+    #M[n-1][n-4] = -2 * h * fFDM / 3
 
     if condition :
         print(np.linalg.cond(M))
@@ -328,15 +330,15 @@ start = 4
 for i in range(start,start+4):
     n = np.power(2,i)
     h = 1./n
-    nodes1 = int(0.75/h)+1
-    nodes2 = int(1.25/h)+1
-    nodes3 = int(1.0/h)+1
+    nodes1 = int(0.5/h)+1
+    nodes2 = int(1.75/h)+1
+    nodes3 = int(0.75/h)+1
     nodesFull = 3 * n + 1
 
     print(nodesFull,h)
-    x1 = np.linspace(0,0.75,nodes1)
-    x2 = np.linspace(0.75,2.,nodes2)
-    x3 = np.linspace(2.,3.,nodes3)
+    x1 = np.linspace(0,0.5,nodes1)
+    x2 = np.linspace(0.5,2.25,nodes2)
+    x3 = np.linspace(2.25,3.,nodes3)
     x = np.array(np.concatenate((x1,x2,x3)))
 
     xFull = np.linspace(0,3.,nodesFull)
@@ -352,8 +354,43 @@ for i in range(start,start+4):
     uFDMVHM = solve(CouplingFDVHM(nodes1,nodes2,nodes3,h),forceCoupled)
     uSlice = np.array(np.concatenate((uFDMVHM[0:nodes1],uFDMVHM[nodes1+1:nodes1+nodes2],uFDMVHM[nodes1+nodes2+1:nodes1+nodes2+nodes3])))
 
-    plt.axvline(x=0.75,c="#536872")
-    plt.axvline(x=2.,c="#536872")
+    uFD = solve(FDM(nodesFull,h),forceFull(nodesFull,h))
+
+    print("------")
+    #print(uFDMVHM[nodes1-1])
+    #print(uFDMVHM[nodes1])
+    #print(uFD[nodes1-1])
+    print("PD")
+    upfd = (-2 * uFDMVHM[nodes1-4]   + 9 *  uFDMVHM[nodes1-3]  - 18 * uFDMVHM[nodes1-2] +  11 * uFDMVHM[nodes1-1] ) / 6
+    uppd = (-2 * uFDMVHM[nodes1+3]   + 9 *  uFDMVHM[nodes1+2]  - 18 * uFDMVHM[nodes1+1] +  11 * uFDMVHM[nodes1] ) / 6
+    print(upfd)
+    print(uppd)
+    print(abs(uppd)-abs(upfd))
+
+    upfdl = (-2 * uFD[nodes1-4]   + 9 *  uFD[nodes1-3]  - 18 * uFD[nodes1-2] +  11 * uFD[nodes1-1] ) / 6
+    upfdr = (-2 * uFD[nodes1+2]   + 9 *  uFD[nodes1+1]  - 18 * uFD[nodes1] +  11 * uFD[nodes1-1] ) / 6
+    print("FDM ")
+    print(upfdl)
+    print(upfdr)
+    print(abs(upfdl)-abs(upfdr))
+
+    print("-- Second interface")
+    upfd = (-2 * uFDMVHM[nodes1+nodes2-4]   + 9 *  uFDMVHM[nodes1+nodes2-3]  - 18 * uFDMVHM[nodes1+nodes2-2] +  11 * uFDMVHM[nodes1+nodes2-1] ) / 6
+    uppd = (-2 * uFDMVHM[nodes1+nodes2+3]   + 9 *  uFDMVHM[nodes1+nodes2+2]  - 18 * uFDMVHM[nodes1+nodes2+1] +  11 * uFDMVHM[nodes1+nodes2] ) / 6
+    print(upfd)
+    print(uppd)
+    print(abs(uppd)-abs(upfd))
+    print("FDM")
+    upfdl = (-2 * uFD[nodes1+nodes2-4]   + 9 *  uFD[nodes1+nodes2-3]  - 18 * uFD[nodes1+nodes2-2] +  11 * uFD[nodes1+nodes2-1] ) / 6
+    upfdr = (-2 * uFD[nodes1+nodes2+2]   + 9 *  uFD[nodes1+nodes2+1]  - 18 * uFD[nodes1+nodes2] +  11 * uFD[nodes1+nodes2-1] ) / 6
+    print(upfdl)
+    print(upfdr)
+    print(abs(upfdl)-abs(upfdr))
+
+    print("-------")
+
+    plt.axvline(x=0.5,c="#536872")
+    plt.axvline(x=2.25,c="#536872")
 
     if example == "Steep":
        
@@ -371,8 +408,9 @@ for i in range(start,start+4):
             
 
         elif solution == "Exact":
-
-            plt.plot(xFull,uSlice-exactSolution(xFull),label=r"LLEM-VHM ($\delta$=1/"+str(int(n/2))+")",c="black",marker=markers[i-start],markevery=n)
+            uFD = solve(FDM(nodesFull,h),forceFull(nodesFull,h))
+            plt.plot(xFull,uFD-exactSolution(xFull),label=r"LLEM-VHM ($\delta$=1/"+str(int(n/2))+")",c="black",marker=markers[i-start],markevery=n)
+            plt.plot(xFull,uSlice-exactSolution(xFull),label=r"LLEM-VHM ($\delta$=1/"+str(int(n/2))+")",c="blue",marker=markers[i-start],markevery=n)
             plt.ylabel("Error in displacement w.r.t. exact solution")
 
     elif i == 4:
